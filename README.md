90eee002dc25e09eda0eeaf69d8f6c16f927b6b3
Francis Ilechukwu <francis94c@gmail.com>
2019-04-19 05:11:58 +0100

    Inital Commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+![Licence MIT](https://img.shields.io/github/license/francis94c/cdn-helper.svg) ![Splint](https://img.shields.io/badge/splint--ci-francis94c%2Fcdn--helper-orange.svg) ![Lates Release](https://img.shields.io/github/release/francis94c/cdn-helper.svg) ![Commits](https://img.shields.io/github/last-commit/francis94c/cdn-helper.svg)
+
+# cdn-helper #
+----
+This library makes available, functions that return HTML link/script tags for popular JS and CSS libraries like
+
+* Bootstrap
+* W3.CSS
+* JQuery
+* Popper.js
---
+++ helpers/cdn_helper.php
@@ -0,0 +1,43 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+if (!function_exists('w3css')) {
+  /**
+   * [w3css description]
+   * @return [type] [description]
+   */
+  function w3css() {
+    return "<link rel=\"stylesheet\" href=\"https://www.w3schools.com/w3css/4/w3.css\" />";
+  }
+}
+if (!function_exists('jquery')) {
+  /**
+   * [jquery description]
+   * @return [type] [description]
+   */
+  function jquery() {
+    return "<script type=\"text/javascript\" src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>";
+  }
+}
+if (!function_exists('bootstrap')) {
+  /**
+   * [bootstrap description]
+   * @param  boolean $js [description]
+   * @return [type]      [description]
+   */
+  function bootstrap($js=false) {
+    $cdn = "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\" integrity=\"sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T\" crossorigin=\"anonymous\">";
+    if ($js) $cdn .= PHP_EOL . "<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js\" integrity=\"sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM\" crossorigin=\"anonymous\"></script>";
+    return $cdn;
+  }
+}
+if (!function_exists("popper")) {
+  /**
+   * [popper description]
+   * @return [type] [description]
+   */
+  function popper() {
+    return "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js\" integrity=\"sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1\" crossorigin=\"anonymous\"></script>";
+  }
+}
+?>
